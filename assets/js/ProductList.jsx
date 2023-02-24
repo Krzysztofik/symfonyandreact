0361e71ca3de18a17993340f08bcbecc3df5e81a
Krzysztofik <fliegekrzysztof@gmail.com>
2023-02-24 13:24:14 +0100

    Database connection works

--- assets/js/ProductList.jsx
+++ assets/js/ProductList.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/products')
+            .then(response => {
+                setProducts(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
     return (
         <div>
-            <h1>PRODUKT</h1>
+            <h1>Product List</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Calories</th>
+                    <th>Protein</th>
+                    <th>Carbohydrates</th>
+                    <th>Fat</th>
+                </tr>
+                </thead>
+                <tbody>
+                {products.map(product => (
+                    <tr key={product.id}>
+                        <td>{product.name}</td>
+                        <td>{product.calories}</td>
+                        <td>{product.protein}</td>
+                        <td>{product.carbohydrates}</td>
+                        <td>{product.fat}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
     );
 };
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,15 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    /**
+     * @Route("/api/products", name="api_products")
+     */
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('products.html.twig');
+        $products = $entityManager->getRepository(Product::class)->findAll();
+
+        $data = [];
+
+        foreach ($products as $product) {
+            $data[] = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'calories' => $product->getCalories(),
+                'protein' => $product->getProtein(),
+                'carbohydrates' => $product->getCarbohydrates(),
+                'fat' => $product->getFat(),
+            ];
+        }
+
+        return $this->json($data);
     }
 }
